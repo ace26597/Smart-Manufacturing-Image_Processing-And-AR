b7d8aae29c29b1c61bf1f0338159cda8c6dd8608
Ankur Chaudhari <chankur26@gmail.com>
2019-05-09 16:56:54 +0530

    Update level_indicator_live.py

--- level_indicator_live.py
+++ level_indicator_live.py
@@ -1,4 +1,6 @@
 # import the necessary packages
+from threading import Thread
+
 import imutils
 import numpy as np
 import cv2
@@ -16,34 +18,12 @@ upper_red = np.array([180,255,255])
 # load the image, convert it to grayscale, and blur it
 initfunc = False
 notdetectedvar = False
-cap = cv2.VideoCapture(0)
-if cap.read()[0] == False:
-    cap = cv2.VideoCapture(1)
-
-def notdetected():
-    global notdetectedvar
-    cap = cv2.VideoCapture(0)
-    if cap.read()[0] == False:
-        cap = cv2.VideoCapture(1)
-    if notdetectedvar == False:
-        for i in range(50):
-            ret, imgg = cap.read()
-            imgg = imutils.resize(imgg, width=700)
-            file = "image_LD.png"
-            cv2.imwrite(file, imgg)
-            cv2.putText(imgg, 'place level indicator in front of camera', (5, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.8,
-                        (255, 0, 0), 2)
-            cv2.imshow('cam', imgg)
-            time.sleep(0.1)
-            k = cv2.waitKey(10)
-            if k == 27:
-                break
-    cap.release()
-    cv2.destroyAllWindows()
-    main()
 
 def i_run_once():
     global initfunc
+    cap = cv2.VideoCapture(0)
+    if cap.read()[0] == False:
+        cap = cv2.VideoCapture(1)
     if initfunc == False:
         for i in range(50):
             ret, imgg = cap.read()
@@ -59,7 +39,7 @@ def i_run_once():
             time.sleep(0.1)
     cap.release()
     cv2.destroyAllWindows()
-    initfunc = True
+    initfunc = False
 
 def ping():
     print("CSV Updated")
@@ -74,14 +54,9 @@ def run_threaded(ping):
 
 schedule.every(10).seconds.do(run_threaded, ping)
 
-def get_image():
- # read is the easiest way to get a full image out of a VideoCapture object.
- retval, im = cap.read()
- file = "image_LD.png"
- cv2.imwrite(file, im)
- return im
 
 def main():
+
     cap = cv2.VideoCapture(0)
     if cap.read()[0] == False:
         cap = cv2.VideoCapture(1)
@@ -90,13 +65,12 @@ def main():
         ret, imgg = cap.read()
         imgg = imutils.resize(imgg, width=500)
         cv2.imshow('cam', imgg)
+        filename = 'image_LD.png'
+        cv2.imwrite(filename,imgg)
         k = cv2.waitKey(10)
         if k == 27:
             break
-        get_image()
-        filename = 'image_LD.png'
         img = cv2.imread('image_LD.png')
-        schedule.every(10).seconds.do(run_threaded, ping)
         ret, image = cap.read()
         image = imutils.resize(image, width=400)
         grayScale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -107,7 +81,6 @@ def main():
         v = np.median(grayScale)
         low = int(max(0, (1.0 - sigma) * v))
         high = int(min(255, (1.0 + sigma) * v))
-
 
         edged = cv2.Canny(grayScale, low, high)
 
@@ -122,49 +95,45 @@ def main():
 
         # loop over the contours
         for c in cnts:
-            # compute the moment of contour
-            M = cv2.moments(c)
-            # From moment we can calculte area, centroid etc
-            # The center or centroid can be calculated as follows
-            #print(M['m00'])
-            if M['m00'] == 0:
-                print('Level Indicator Not Found \n 5 second window to place Level indicator in front of camera')
-                cap.release()
-                cv2.destroyAllWindows()
-                notdetectedvar = False
-                notdetected()
+            try:
+                # compute the moment of contour
+                M = cv2.moments(c)
+                # From moment we can calculte area, centroid etc
+                # The center or centroid can be calculated as follows
+                # print(M['m00'])
+                if M['m00'] == 0:
+                    main()
+                cX = int(M['m10'] / M['m00'])
+                cY = int(M['m01'] / M['m00'])
+                area = cv2.contourArea(c)
+                # print('area : ', area)
+                perimeter = cv2.arcLength(c, True)
+                # print('peremeter : ',perimeter)
+                # Outline the contours
+                cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+                x, y, w, h = cv2.boundingRect(c)  # offsets - with this you get 'mask'
+                cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                # cv2.imshow('cutted contour', image[y:y + h, x:x + w])
+                crop_img = image[y:y + h, x:x + w]
+                # show the output image
 
-            cX = int(M['m10'] / M['m00'])
-            cY = int(M['m01'] / M['m00'])
-            area = cv2.contourArea(c)
-            #print('area : ', area)
-            perimeter = cv2.arcLength(c, True)
-            #print('peremeter : ',perimeter)
-            # Outline the contours
-            cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-            x, y, w, h = cv2.boundingRect(c)  # offsets - with this you get 'mask'
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            #cv2.imshow('cutted contour', image[y:y + h, x:x + w])
-            crop_img = image[y:y + h, x:x + w]
-            # show the output image
-
-            hsv = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
-            mask0 = cv2.inRange(hsv, lower_range, upper_range)
-            mask1 = cv2.inRange(hsv, lower_red, upper_red)
-            size = crop_img.size / 3
-            mask = mask0 + mask1
-            blurred = cv2.GaussianBlur(mask, (11, 11), 0)
-            PixelsInRange = cv2.countNonZero(mask)
-            #print("PixelsInRange", PixelsInRange)
-            frac_red = np.divide(float(PixelsInRange), int(size))
-            #print(frac_red)
-            percent_red = np.multiply((float(frac_red)), 100)
-            print('Level : ' + str(percent_red) + '%')
-            #cv2.imshow('mask',mask)
-            data = [[percent_red]]
-
-        cv2.waitKey(0)
-        schedule.run_pending()
+                hsv = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
+                mask0 = cv2.inRange(hsv, lower_range, upper_range)
+                mask1 = cv2.inRange(hsv, lower_red, upper_red)
+                size = area - perimeter
+                # print('size ', size)
+                mask = mask0 + mask1
+                PixelsInRange = cv2.countNonZero(mask)
+                div = PixelsInRange + perimeter
+                # print('div ', div)
+                frac_red = np.divide(float(div), int(size))
+                percent_red = np.multiply((float(frac_red)), 100)
+                print('Level : ' + str(percent_red) + '%')
+                data = [percent_red]
+            except Exception as e:
+                None
+        
+    schedule.run_pending()
 
 if __name__=='__main__':
     i_run_once()
